2b7291f27b3273fbe476216d3f8e62ae63f11bb1
Michael <michaelmwendwamumo@gmail.com>
2024-04-25 12:12:44 +0300

    models created and API endpoints.

--- server/app.py
+++ server/app.py
@@ -8,18 +8,158 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        image_url = data.get('image_url')
+        bio = data.get('bio')
+        password = data.get('password')
+        password_confirmation = data.get('password_confirmation')
+
+        existing_user = User.query.filter(User.username == username).first()
+
+        if existing_user:
+            return (
+                {'error': 'Username already exists'},
+                422,
+            )        
+
+        if not all ([username, image_url, bio, password, password_confirmation]):
+            return (
+                {'error': 'All fields are required to be filled'},
+                422,
+            )
+
+        if password_confirmation != password:
+            return (
+                {'error': 'Password confirmation does not match password'},
+                422,
+            )
+
+        user = User(username=username, image_url=image_url, bio=bio)
+        user.password_hash(password)
+        session['user_id'] = user.id
+
+        return (
+            user.to_dict(),
+            201,
+        )
+
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        ID = session['user_id']
+
+        if ID:
+            user = User.query.filter(User.id == ID).first()
+
+            return ({
+                user.to_dict(),
+                200,
+            })
+        else:
+            return (
+                {'error': 'User not logged in.'},
+                401,
+            )
+
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if not user:
+            return (
+                {'error': 'Username not found.'},
+                401,
+            )
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return (
+                user.to_dict(),
+                200,
+            )
+        else:
+            return (
+                {'error': 'Incorrect password'},
+                401,
+            )
 
 class Logout(Resource):
-    pass
+
+    def delete(self):
+        if session['user_id']:
+            session['user_id'] = None
+            return (
+                {},
+                204,
+            )
+        else:
+            return (
+                {'error': 'User must be logged in.'},
+                401,
+            )
+
 
 class RecipeIndex(Resource):
+
+    def get(self):
+        ID = session['user_id']
+        
+        if ID:
+            recipes = []
+            for recipe in Recipe.query.all():
+                recipe.user_id = ID
+                recipe_dict = recipe.to_dict()
+                recipes += recipe_dict
+            return (
+                recipes,
+                200,
+            )
+        else:
+            return (
+                {'error': 'User must be logged in.'},
+                401,
+            )
+        
+    def post(self):
+        ID = session['user_id']
+        data = request.get_json()
+        title = data.get('title')
+        instructions = data.get('instructions')
+        minutes_to_complete = data.get('minutes_to_complete')
+        
+        if ID:
+            user = User.query.filter(User.id == ID).first()
+            if not all([data, title, instructions, minutes_to_complete]):
+                return (
+                    {'error': 'All fields must be filled'},
+                    422,
+                )
+            new_recipe = Recipe(title=title, instructions=instructions, minutes_to_complete=minutes_to_complete, user_id=ID)
+            
+            return (
+                new_recipe.to_dict(),
+                201
+            )
+        
+        else:
+            return (
+                {'error': 'User must be logged in.'},
+                422,
+            )
+
+
+
+
     pass
 
 api.add_resource(Signup, '/signup', endpoint='signup')
--- server/models.py
+++ server/models.py
@@ -6,9 +6,45 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates='user')
+
+    serialize_rules = ('-recipes.user',)
+
+    def __repr__(self):
+        return f"<User {self.id}, {self.username}, {self.image_url}, {self.bio}>"
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password is a secret.")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+        
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, db.CheckConstraint('len(instructions) > 50') ,nullable=False)
+
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates='recipes')
+
+    serialize_rules = ('-user.recipes',)
+
+    def __repr__(self):
+        return f"<Recipe {self.id}, {self.title}, {self.instructions}, {self.minutes_to_complete}>"
     
-    pass
